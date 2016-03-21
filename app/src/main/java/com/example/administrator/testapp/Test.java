965e0b1d0a26c09646b7ade7edbf12bb60d82597
fianlly <407864613@qq.com>
2016-03-21 16:11:26 +0800

    Update Test.java

--- app/src/main/java/com/example/administrator/testapp/Test.java
+++ app/src/main/java/com/example/administrator/testapp/Test.java
@@ -4,5 +4,7 @@ package com.example.administrator.testapp;
  * Created by Administrator on 2016/3/21 0021.
  */
 public class Test {
-
+    public static void main(String srg[]){
+        System.out.println("2");
+    }
 }
