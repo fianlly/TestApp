54e9e377bb7b57d1477c6b3af6e1213301908d70
fianlly <407864613@qq.com>
2016-03-21 16:09:14 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/administrator/testapp/Test.java

--- app/src/main/java/com/example/administrator/testapp/Test.java
+++ app/src/main/java/com/example/administrator/testapp/Test.java
@@ -4,7 +4,5 @@ package com.example.administrator.testapp;
  * Created by Administrator on 2016/3/21 0021.
  */
 public class Test {
-      public static void main(String srg[]){
-        System.out.println("2");
-    }
+
 }
