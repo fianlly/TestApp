6072064995d61abf82b9d22f5bdcb8f0679f9888
fianlly <407864613@qq.com>
2016-03-21 15:57:11 +0800

    Update Test.java

--- app/src/main/java/com/example/administrator/testapp/Test.java
+++ app/src/main/java/com/example/administrator/testapp/Test.java
@@ -4,4 +4,7 @@ package com.example.administrator.testapp;
  * Created by Administrator on 2016/3/21 0021.
  */
 public class Test {
+  public static void main(String arg[]){
+        System.out.println("远程代码");
+    }
 }
